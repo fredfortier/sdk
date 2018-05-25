eb05f58e77fd13767e27b56be070be15e3dd0624
michaelharrisonroth <hi@mikeroth.io>
2018-05-24 18:04:38 -0600

    Fix uncaught fs file load error

--- src/radar-relay.ts
+++ src/radar-relay.ts
@@ -103,7 +103,7 @@ export class RadarRelay {
         try {
           wallet = await walletManager.core.loadWalletAsync(config.wallet.password);
         } catch (err) {
-          if (err.message === 'NO_WALLET_FOUND') {
+          if (err.message === 'NO_WALLET_FOUND' || err.message.indexOf('no such file or directory') > 0) {
             // create a new core wallet
             wallet = await walletManager.core.createWalletAsync(config.wallet);
           } else {
