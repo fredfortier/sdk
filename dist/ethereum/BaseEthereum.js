0f2e24b658573a3f97e1a6a7b2c3a380c8120b22
Cavan <cavan@radarrelay.com>
2018-06-07 23:03:41 -0600

    Clean up dist

--- dist/ethereum/BaseEthereum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var BaseEthereum = /** @class */ (function () {
-    function BaseEthereum() {
-    }
-    return BaseEthereum;
-}());
-exports.BaseEthereum = BaseEthereum;
--- dist/ethereum/LocalEthereum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var LocalEthereum = /** @class */ (function () {
-    function LocalEthereum() {
-    }
-    return LocalEthereum;
-}());
-exports.LocalEthereum = LocalEthereum;
